87de649b4305de458b9ed1f12f39a2b7beda41a0
Mel Collins <mel@raumkraut.net>
2018-09-13 20:30:57 +0200

    Rebase FollowOther onto same base as LookAtOther

--- addons/followother/FollowOther.gd
+++ addons/followother/FollowOther.gd
@@ -1,75 +1,17 @@
 # Node which keeps its parent transform the same as the target node's
 #
-# NB. Currently mirroring rotation also mirrors scale. Because Basis.
+# NB. Currently mirroring rotation ignores scale. Because Basis.
 tool
-extends Node
+extends "BaseOther.gd"
 
 # Configuration
-export(NodePath) var target_path setget set_target_path
 export(bool) var follow_x = true
 export(bool) var follow_y = true
 export(bool) var follow_z = true
 export(bool) var follow_rotation = true
-export(float) var interpolate_speed = 0
-export(bool) var physics_sync = false setget set_physics_sync
 
-var parent
-var target setget set_target
 
-func set_target_path(val):
-	# Sets a new target NodePath
-	target_path = val
-	if target_path != null and has_node(target_path):
-		set_target(get_node(target_path))
-	else:
-		target = null
-	
-func set_target(val):
-	# Sets or clears a new target Spatial node
-	
-	if val == null:
-		# Wipe out the stored values
-		target_path = null
-		target = null
-		return
-	
-	# Otherwise, make sure we have what we expect
-	if typeof(val) == TYPE_OBJECT and val is Spatial:
-		pass
-	else:
-		return
-	
-	target_path = get_path_to(val)
-	target = val
-	# Active tool scripts have all their vars wiped when saved,
-	# but _ready doesn't get called. So we fetch parent again here.
-	parent = get_parent()
-	
-func set_physics_sync(val):
-	val = bool(val)
-	physics_sync = val
-	
-	if Engine.editor_hint:
-		# The editor doesn't process physics
-		val = false
-	set_physics_process(val)
-	set_process(not val)
-	
-
-func _ready():
-	set_physics_sync(physics_sync)
-	set_target_path(target_path)
-	
-
-func _process(delta):
-	do_process(delta)
-func _physics_process(delta):
-	do_process(delta)
-	
 func do_process(delta):
-	if target == null or parent == null:
-		return
-	
 	if follow_x or follow_y or follow_z:
 		mirror_position(delta)
 	if follow_rotation:
