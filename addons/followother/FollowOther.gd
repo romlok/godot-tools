2b6769b5ff81954737a12f666c6fb3e532cfe5a6
Mel Collins <mel@raumkraut.net>
2018-09-13 14:14:11 +0200

    Split position follow to axis-specific

--- addons/followother/FollowOther.gd
+++ addons/followother/FollowOther.gd
@@ -6,8 +6,10 @@ extends Node
 
 # Configuration
 export(NodePath) var target_path setget set_target_path
-export(bool) var mirror_position = true
-export(bool) var mirror_rotation = true
+export(bool) var follow_x = true
+export(bool) var follow_y = true
+export(bool) var follow_z = true
+export(bool) var follow_rotation = true
 export(float) var interpolate_speed = 0
 export(bool) var physics_sync = false setget set_physics_sync
 
@@ -68,26 +70,39 @@ func do_process(delta):
 	if target == null or parent == null:
 		return
 	
-	var diff
-	if mirror_position:
-		diff = parent.global_transform.origin.distance_to(target.global_transform.origin)
-		if interpolate_speed > 0.001 and diff > 0.001:
-			parent.global_transform.origin = parent.global_transform.origin.linear_interpolate(
-				target.global_transform.origin,
-				interpolate_speed * delta
-			)
-		elif diff > 0:
-			parent.global_transform.origin = target.global_transform.origin
-		
-	if mirror_rotation:
-		var our_quat = Quat(parent.global_transform.basis)
-		var target_quat = Quat(target.global_transform.basis)
-		diff = (our_quat + -target_quat).length_squared()
-		if interpolate_speed > 0.001 and diff > 0.000001:
-			parent.global_transform.basis = Basis(
-				our_quat.slerp(target_quat, interpolate_speed * delta)
-			)
-		elif diff > 0:
-			parent.global_transform.basis = target.global_transform.basis
-		
+	if follow_x or follow_y or follow_z:
+		mirror_position(delta)
+	if follow_rotation:
+		mirror_rotation(delta)
+	
+func mirror_position(delta):
+	# Work out he actual point we're to follow
+	var parent_pos = parent.global_transform.origin
+	var target_pos = Vector3(target.global_transform.origin)
+	if not follow_x:
+		target_pos.x = parent_pos.x
+	if not follow_y:
+		target_pos.y = parent_pos.y
+	if not follow_z:
+		target_pos.z = parent_pos.z
+	
+	var diff = parent_pos.distance_to(target_pos)
+	if interpolate_speed > 0.001 and diff > 0.001:
+		parent.global_transform.origin = parent_pos.linear_interpolate(
+			target_pos,
+			interpolate_speed * delta
+		)
+	elif diff > 0:
+		parent.global_transform.origin = target_pos
+	
+func mirror_rotation(delta):
+	var our_quat = Quat(parent.global_transform.basis)
+	var target_quat = Quat(target.global_transform.basis)
+	var diff = (our_quat + -target_quat).length_squared()
+	if interpolate_speed > 0.001 and diff > 0.000001:
+		parent.global_transform.basis = Basis(
+			our_quat.slerp(target_quat, interpolate_speed * delta)
+		)
+	elif diff > 0:
+		parent.global_transform.basis = target.global_transform.basis
 	
