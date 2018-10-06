0741f25a3e5ab349637d51a153f2104e44febec0
Mel Collins <mel@raumkraut.net>
2018-10-06 12:43:03 +0200

    Allow LookAtOther without changing pitch

--- addons/followother/LookAtOther.gd
+++ addons/followother/LookAtOther.gd
@@ -3,19 +3,25 @@ tool
 extends "BaseOther.gd"
 
 export(Vector3) var up_vector = Vector3(0, 1, 0)
-export(bool) var negative_z = true # 
+export(bool) var enable_pitching = true
+export(bool) var negative_z = true
 
 
 func do_process(delta):
-	# Make the parent look at the target
+	# First work out where we're supposed to be looking
 	var parent_trans = get_parent_global_transform()
+	var target_point = get_target_global_transform().origin
+	if not enable_pitching:
+		# Find the equivalent point on our horizontal plane
+		target_point = target_point.slide(up_vector) + parent_trans.origin
 	var new_basis = parent_trans.looking_at(
-		get_target_global_transform().origin, up_vector
+		target_point, up_vector
 	).basis
 	if not negative_z:
 		# We want to look with our +Z axis
 		new_basis = new_basis.rotated(new_basis.y, PI)
 	
+	# Now make the parent actually look at the target
 	var parent_quat = Quat(parent_trans.basis)
 	var target_quat = Quat(new_basis)
 	var diff = (parent_quat + -target_quat).length_squared()
