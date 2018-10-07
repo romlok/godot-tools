bb4185091b635ed3a49e4d746b9979398637dc0f
Mel Collins <mel@raumkraut.net>
2018-10-07 15:40:44 +0200

    Refactor and simplify ConstrainRotation logic

--- addons/followother/ConstrainRotation.gd
+++ addons/followother/ConstrainRotation.gd
@@ -1,4 +1,4 @@
-# Node which keeps its parent transform within certain bounds
+# Node which keeps its parent facing within certain bounds
 tool
 extends "BaseParentAffecter.gd"
 
@@ -11,33 +11,44 @@ var up_vector = Vector3(0, 1, 0)
 
 func do_process(delta):
 	var parent_trans = get_parent_global_transform()
+	var basis = parent_trans.basis
 	var rest_basis = get_parent_global_rest_transform().basis
-	if max_pitch_degrees > 0:
-		# Constrain in the YZ plane
-		var basis = parent_trans.basis
-		var rest_plane = rest_basis.y
-		var current_angle = basis.z.angle_to(basis.z.slide(rest_plane))
-		if current_angle > deg2rad(max_pitch_degrees):
-			# Move us to the max_pitch degrees
-			var perp_normal = basis.z.cross(rest_plane).normalized()
-			var direction = -sign(basis.z.dot(rest_plane))
-			parent_trans.basis = basis.rotated(perp_normal, (current_angle - deg2rad(max_pitch_degrees)) * direction)
-			
-		
 	if max_yaw_degrees > 0:
 		# Constrain in the XZ plane
-		var xz_forward = -rest_basis.z.slide(up_vector).normalized()
-		var xz_facing = -parent_trans.basis.z.slide(up_vector).normalized()
-		# Work out which way to rotate the max vector
-		# A positive facing_side means the target is clockwise of -rest_basis.z
-		var facing_side = sign(xz_facing.dot(rest_basis.x))
-		var max_angle = deg2rad(max_yaw_degrees) * -facing_side
-		var xz_max = xz_forward.rotated(up_vector, max_angle)
+		var correction = get_constraint_correction(
+			basis.z,
+			rest_basis.z,
+			rest_basis.y.normalized(),
+			deg2rad(max_yaw_degrees)
+		)
+		if correction:
+			basis = basis.rotated(rest_basis.y, correction)
 		
-		if xz_facing.dot(xz_forward) < xz_max.dot(xz_forward):
-			# We're further from forward than the max
-			var angle = xz_facing.angle_to(xz_max)
-			parent_trans.basis = parent_trans.basis.rotated(up_vector, angle * facing_side)
-	
+	if max_pitch_degrees > 0:
+		# Constrain in the YZ plane
+		var correction = get_constraint_correction(
+			basis.z,
+			rest_basis.z,
+			basis.x.normalized(),
+			deg2rad(max_pitch_degrees)
+		)
+		if correction:
+			basis = basis.rotated(basis.x.normalized(), correction)
+		
+	parent_trans.basis = basis
 	set_parent_global_transform(parent_trans)
+	
+func get_constraint_correction(vector, rest_vector, axis_normal, max_radians):
+	# Returns the correction needed to keep the `vector` within
+	# `max_radians` around `axis_normal` of the `rest_vector`.
+	var current_angle = vector.slide(axis_normal).angle_to(
+			rest_vector.slide(axis_normal)
+	)
+	var extra_angle = current_angle - max_radians
+	if extra_angle <= 0:
+		# Within bounds, nothing needs to be done
+		return 0
+	# Work out which direction around the axis we need to rotate
+	var direction = sign(vector.dot(rest_vector.cross(axis_normal)))
+	return extra_angle * direction
 	
