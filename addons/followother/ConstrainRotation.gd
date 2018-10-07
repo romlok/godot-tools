d4da78e8cfa068da72c7e8114d8241970af3e74f
Mel Collins <mel@raumkraut.net>
2018-10-07 13:56:14 +0200

    Add ability to constrain pitch

--- addons/followother/ConstrainRotation.gd
+++ addons/followother/ConstrainRotation.gd
@@ -13,8 +13,17 @@ func do_process(delta):
 	var parent_trans = get_parent_global_transform()
 	var rest_basis = get_parent_global_rest_transform().basis
 	if max_pitch_degrees > 0:
-		# Constrain in the ZY plane
-		pass
+		# Constrain in the YZ plane
+		var basis = parent_trans.basis
+		var rest_plane = rest_basis.y
+		var current_angle = basis.z.angle_to(basis.z.slide(rest_plane))
+		if current_angle > deg2rad(max_pitch_degrees):
+			# Move us to the max_pitch degrees
+			var perp_normal = basis.z.cross(rest_plane).normalized()
+			var direction = -sign(basis.z.dot(rest_plane))
+			parent_trans.basis = basis.rotated(perp_normal, (current_angle - deg2rad(max_pitch_degrees)) * direction)
+			
+		
 	if max_yaw_degrees > 0:
 		# Constrain in the XZ plane
 		var xz_forward = -rest_basis.z.slide(up_vector).normalized()
@@ -29,6 +38,6 @@ func do_process(delta):
 			# We're further from forward than the max
 			var angle = xz_facing.angle_to(xz_max)
 			parent_trans.basis = parent_trans.basis.rotated(up_vector, angle * facing_side)
-			set_parent_global_transform(parent_trans)
-		
+	
+	set_parent_global_transform(parent_trans)
 	
