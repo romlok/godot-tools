653376ccdae954eaeeea480153b60f4bf12ef6de
Mel Collins <mel@raumkraut.net>
2019-04-21 12:17:05 +0200

    Fix MouselookController for Godot 3.1

--- addons/inputcontrols/MouselookController.gd
+++ addons/inputcontrols/MouselookController.gd
@@ -13,8 +13,9 @@ var mouselook_hold_timer
 # We expose the min/max pitches as degrees, but use rads
 export(float) var min_pitch = -89.9 setget set_min_pitch
 export(float) var max_pitch = 89.9 setget set_max_pitch
-var mouselook_scale_rads = Vector2()
-var pitch_limits = [null, null]
+var mouselook_scale_rads = Vector2(PI / 180, PI / 180)
+# Remember: Min is max and max is min because forward is -z
+var pitch_limits = [pitch2rad(max_pitch), pitch2rad(min_pitch)]
 var last_mouse_pos = Vector2()
 var used_actions = [
 	"mouselook_toggle",
@@ -41,15 +42,17 @@ func set_mouselook_scale(val):
 	# The exposed var is set in degrees turned per unit moused
 	mouselook_scale_rads = mouselook_scale * (PI / 180)
 	
+func pitch2rad(val):
+	# Flip pitch because forward is -z
+	return -deg2rad(val)
 func set_min_pitch(val):
 	min_pitch = val
 	# Remember: min is max because forward is -z
-	pitch_limits[1] = -deg2rad(val)
-	
+	pitch_limits[1] = pitch2rad(val)
 func set_max_pitch(val):
 	max_pitch = val
 	# Remember: max is min because forward is -z
-	pitch_limits[0] = -deg2rad(val)
+	pitch_limits[0] = pitch2rad(val)
 	
 
 func _ready():
@@ -97,10 +100,12 @@ func _unhandled_input(event):
 		new_y -= event.relative.x * mouselook_scale_rads.x
 		new_x -= event.relative.y * mouselook_scale_rads.y
 		# Make sure we don't go upside-down
-		if new_x < pitch_limits[0]:
-			new_x = pitch_limits[0]
-		elif new_x > pitch_limits[1]:
-			new_x = pitch_limits[1]
+		var pitch_lower = pitch_limits[0]
+		var pitch_upper = pitch_limits[1]
+		if pitch_lower != null and new_x < pitch_lower:
+			new_x = pitch_lower
+		elif pitch_upper != null and new_x > pitch_upper:
+			new_x = pitch_upper
 		
 		parent.rotation.x = new_x
 		parent.rotation.y = new_y
